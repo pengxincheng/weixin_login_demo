d6832c4fd7f2770c2d650e0f962182a4e0c3b8a5
彭新成 <pengxincheng@ipaynow.cn>
2018-11-20 14:43:22 +0800

    初始化

---
+++ src/main/java/com/pxc/weixin_login_demo/WeixinLoginDemoApplication.java
@@ -0,0 +1,12 @@
+package com.pxc.weixin_login_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinLoginDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeixinLoginDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/pxc/weixin_login_demo/WeixinLoginDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pxc.weixin_login_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeixinLoginDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
