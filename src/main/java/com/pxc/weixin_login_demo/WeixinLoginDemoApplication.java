e5b61377f0f4538da28bfd8f21174b6f8d4c315d
彭新成 <pengxincheng@ipaynow.cn>
2018-11-21 19:43:16 +0800

    登录

--- src/main/java/com/pxc/weixin_login_demo/WeixinLoginDemoApplication.java
+++ src/main/java/com/pxc/weixin_login_demo/WeixinLoginDemoApplication.java
@@ -1,9 +1,11 @@
 package com.pxc.weixin_login_demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.pxc.weixin_login_demo.dao")
 public class WeixinLoginDemoApplication {
 
     public static void main(String[] args) {
