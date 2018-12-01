d3c9c5d59512c23b33a9cba32ac85af1a9429e19
xinchegn.peng <871666430@qq.com>
2018-12-01 15:03:09 +0800

    fix

--- src/main/java/com/pxc/weixin_login_demo/config/WxConfig.java
+++ src/main/java/com/pxc/weixin_login_demo/config/WxConfig.java
@@ -6,6 +6,7 @@ package com.pxc.weixin_login_demo.config;
  * @Time 17:42
  */
 public interface WxConfig {
+
     String APP_ID = "wx496ae3c06bd9638c";
     String APP_SECRET = "438bf939ea9ac51713bfab74a0c27909";
     String TOKEN = "pxc";
